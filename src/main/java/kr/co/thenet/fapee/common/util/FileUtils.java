1dd4d5d4ef2c535f6a339f2a5acd6d966e2b7eff
thenet <>
2019-08-27 10:12:56 +0900

    Upload image // Todo : It needs to modify S3 util function

--- src/main/java/kr/co/thenet/fapee/common/util/FileUtils.java
+++ src/main/java/kr/co/thenet/fapee/common/util/FileUtils.java
@@ -2,11 +2,41 @@ package kr.co.thenet.fapee.common.util;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
 
 import org.springframework.web.multipart.MultipartFile;
 
 public class FileUtils {
 
+	public static String uploadFiles(List<MultipartFile> files){
+		
+		String pathPrefix = "images/";
+		String result = "success";
+		
+		if(files.isEmpty()) {
+			result = "List Empty";
+			return result;
+		}
+
+		for(MultipartFile multipartFile : files) {
+			String originFileName = System.currentTimeMillis() + "_" + multipartFile.getOriginalFilename();
+			String savePath = pathPrefix + getHashDir(originFileName);
+			File directory = new File(savePath);
+			if(!directory.exists()) {
+				directory.mkdirs();
+			}
+			
+			String destinationFile = savePath + originFileName;
+			
+			try {
+				multipartFile.transferTo( new File(destinationFile) );
+			} catch (IllegalStateException | IOException e) {
+				result = "Exception";
+			}
+		}
+		return result;
+	}
+	
 	public static File MultipartToFile(MultipartFile multipartFile) throws IllegalStateException, IOException {
 		File convertFile = new File(multipartFile.getOriginalFilename());
 		multipartFile.transferTo(convertFile);
--- src/main/java/kr/co/thenet/fapee/look/web/LookController.java
+++ src/main/java/kr/co/thenet/fapee/look/web/LookController.java
@@ -2,14 +2,22 @@ package kr.co.thenet.fapee.look.web;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 import kr.co.thenet.fapee.common.util.EgovMap;
+import kr.co.thenet.fapee.common.util.FileUtils;
 import kr.co.thenet.fapee.look.service.LookService;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Controller
 public class LookController {
 
@@ -41,5 +49,22 @@ public class LookController {
 	public String lookList() throws Exception {
 		
 		return "look/list.admin";
+	}
+	
+	@GetMapping("/admin/look/add.do")
+	public String lookAdd() throws Exception {
+		return "look/add.admin";
+	}
+	
+	@PostMapping("/admin/look/add.do")
+	public String lookAdd(@RequestParam("pro-image") List<MultipartFile> multipartFiles, HttpServletRequest request, ModelMap model) throws Exception {
+		
+		//TODO: 업로드 성공, "ex) 디렉토리형식 업로드도 가능(/images/ab/ab/)"
+//		S3Utils.init();
+//		S3Utils.uploadFile("1.png", FileUtils.MultipartToFile(file));
+
+		FileUtils.uploadFiles(multipartFiles);
+		
+		return "look/add.admin";
 	}
 }
