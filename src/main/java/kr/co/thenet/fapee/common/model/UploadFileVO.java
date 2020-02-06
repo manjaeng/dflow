54f62f54c35b7f18157450bf77678e26cfbf0c86
KR00K00 <gmrtks@gmail.com>
2020-02-07 01:17:34 +0900

    업로드시 이미지 파일 중 썸네일을 찾아서 비디오 파일 정보에 같이 리턴하도록 변경.

--- src/main/java/kr/co/thenet/fapee/common/model/UploadFileVO.java
+++ src/main/java/kr/co/thenet/fapee/common/model/UploadFileVO.java
@@ -2,6 +2,7 @@ package kr.co.thenet.fapee.common.model;
 
 import java.util.List;
 
+import org.apache.commons.io.FilenameUtils;
 import org.springframework.web.multipart.MultipartFile;
 
 import lombok.Getter;
@@ -12,5 +13,22 @@ import lombok.ToString;
 @Setter
 @ToString
 public class UploadFileVO {
+	private int seq;
+	private String type;
+	private String fileName;
+	private String savedPath;
+	private boolean isThumbnail;
+	private String thumbnailPath;
+	
 	private List<MultipartFile> files;
+
+    public static UploadFileVO of(String fileName, String savedPath, String type) {
+    	UploadFileVO fileItem = new UploadFileVO();
+    	fileItem.setFileName(fileName);
+    	fileItem.setSavedPath(savedPath);
+    	fileItem.setType(type);
+    	
+        return fileItem;
+    }
+
 }
--- src/main/java/kr/co/thenet/fapee/common/web/UploadRestController.java
+++ src/main/java/kr/co/thenet/fapee/common/web/UploadRestController.java
@@ -13,13 +13,16 @@ import javax.servlet.ServletContext;
 import javax.servlet.http.HttpSession;
 
 import org.apache.commons.io.FilenameUtils;
+import org.apache.tika.Tika;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import kr.co.thenet.fapee.common.model.RestResultVO;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
 import kr.co.thenet.fapee.common.model.UploadFileVO;
 import lombok.extern.slf4j.Slf4j;
 
@@ -38,6 +41,7 @@ public class UploadRestController {
 	 * @throws Exception
 	 */
 	@PostMapping("/rest/file_upload.do")
+	@JsonInclude(Include.NON_EMPTY)
 	public Map<String, Object> fileUpload(UploadFileVO form, HttpSession session) throws Exception {
 		Map<String, Object> result = new HashMap<String, Object>();
 		
@@ -48,19 +52,18 @@ public class UploadRestController {
 			String uploadPath = String.format("/upload-files/%s", LocalDate.now().format(DateTimeFormatter.ofPattern("yyyyMMdd")));
 			
 			String savedPath = null;
-			int cnt = 0;
-			List<Map<String, Object>> fileList = new ArrayList<Map<String, Object>>();
-			Map<String, Object> fileMap = null;
+			List<UploadFileVO> fileList = new ArrayList<UploadFileVO>();
 			for(MultipartFile mfile : form.getFiles()) {
-				fileMap = new HashMap<String, Object>();
-				cnt++;
 				savedPath = String.format("%s/%s.%s"
 												, uploadPath
 												, UUID.randomUUID().toString()
 												, FilenameUtils.getExtension(mfile.getOriginalFilename()));
 				String fileFullPath = String.format("%s%s", basePath, savedPath).replaceAll("//", "/");
 				
-				log.info("...{}", mfile.getContentType());
+			    String mimeType = new Tika().detect(mfile.getBytes());
+			    String contentType = (mimeType!=null && mimeType.startsWith("video"))? "video" : "image";
+
+				log.info("...{}", contentType);
 				log.info("...{}", mfile.getOriginalFilename());
 				log.info("...{}", savedPath);
 				log.info("...{}", fileFullPath);
@@ -71,15 +74,39 @@ public class UploadRestController {
 					file.getParentFile().mkdirs();
 				}
 				mfile.transferTo(file);
-				
-				fileMap.put("seq", cnt);
-				fileMap.put("fileName", mfile.getOriginalFilename());
-				fileMap.put("savedPath", savedPath);
-				fileList.add(fileMap);
+			    
+				fileList.add(UploadFileVO.of(mfile.getOriginalFilename(), savedPath, contentType));
+			}
+
+			//비디오 파일들에 대해 썸네일 이미지를 찾아서 설정.
+			fileList.stream()
+				.filter(item -> "video".equals(item.getType()))
+				.forEach(video -> {
+					String videoName = FilenameUtils.getBaseName(video.getFileName());
+					UploadFileVO thumbnail = fileList.stream().filter(thumb -> {
+						String thumbName = String.format("%s.png", videoName);	//비디오명에 png를 붙여 썸네일을 찾는다.
+						return thumbName.equals(thumb.getFileName());
+					}).findFirst().orElse(null);
+					
+					//썸네일인 경우 해당 경로를 비디오의 썸네일 경로에 설정한다.
+					if(thumbnail != null) {
+						thumbnail.setThumbnail(true);
+						video.setThumbnailPath(thumbnail.getSavedPath());
+					}
+				});
+			
+			//최종 목록 계산.
+			List<UploadFileVO> finalList = new ArrayList<UploadFileVO>();
+			int i = 0;
+			for(UploadFileVO item : fileList) {
+				if(item.isThumbnail()) continue;
+				i++;
+				item.setSeq(i);
+				finalList.add(item);
 			}
 			
 			result.put("result", 0);
-			result.put("list", fileList);
+			result.put("list", finalList);
 		}
 		catch(Exception e) {
 			log.error("ERROR-upload...{}", e);
