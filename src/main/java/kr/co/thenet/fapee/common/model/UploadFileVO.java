f1339e539f08d9430b374e901dad9c8b7a91ce0c
KR00K00 <gmrtks@gmail.com>
2020-02-09 17:39:36 +0900

    파일 업로드 수정

--- src/main/java/kr/co/thenet/fapee/common/model/UploadFileVO.java
+++ src/main/java/kr/co/thenet/fapee/common/model/UploadFileVO.java
@@ -1,8 +1,8 @@
 package kr.co.thenet.fapee.common.model;
 
+import java.util.ArrayList;
 import java.util.List;
 
-import org.apache.commons.io.FilenameUtils;
 import org.springframework.web.multipart.MultipartFile;
 
 import lombok.Getter;
@@ -13,24 +13,29 @@ import lombok.ToString;
 @Setter
 @ToString
 public class UploadFileVO {
-	private int seq;
-	private String type;
-	private String mimeType;
-	private String fileName;
-	private String savedPath;
-	private String thumbnailPath;
 	
-	private List<MultipartFile> files;
-
-    public static UploadFileVO of(String fileName, String savedPath, String type, String mimeType, int seq) {
-    	UploadFileVO fileItem = new UploadFileVO();
-    	fileItem.setFileName(fileName);
-    	fileItem.setSavedPath(savedPath);
-    	fileItem.setType(type);
-    	fileItem.setMimeType(mimeType);
-    	fileItem.setSeq(seq);
-    	
-        return fileItem;
-    }
+	private List<MultipartFile> files = new ArrayList<MultipartFile>();
+	
+	private MultipartFile param00;
+	private MultipartFile param01;
+	private MultipartFile param02;
+	private MultipartFile param03;
+	private MultipartFile param04;
+	private MultipartFile param05;
+	private MultipartFile param06;
+	private MultipartFile param07;
+	private MultipartFile param08;
+	private MultipartFile param09;
+	
+	private MultipartFile param10;
+	private MultipartFile param11;
+	private MultipartFile param12;
+	private MultipartFile param13;
+	private MultipartFile param14;
+	private MultipartFile param15;
+	private MultipartFile param16;
+	private MultipartFile param17;
+	private MultipartFile param18;
+	private MultipartFile param19;
 
 }
--- src/main/java/kr/co/thenet/fapee/common/web/UploadRestController.java
+++ src/main/java/kr/co/thenet/fapee/common/web/UploadRestController.java
@@ -1,6 +1,5 @@
 package kr.co.thenet.fapee.common.web;
 
-import java.io.File;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
@@ -24,6 +23,7 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 
 import kr.co.thenet.fapee.common.model.UploadFileVO;
+import kr.co.thenet.fapee.common.model.UploadResultVO;
 import kr.co.thenet.fapee.common.util.Constants;
 import kr.co.thenet.fapee.common.util.S3Utils;
 import lombok.extern.slf4j.Slf4j;
@@ -47,11 +47,36 @@ public class UploadRestController {
 	public Map<String, Object> fileUpload(UploadFileVO form, HttpSession session) throws Exception {
 		Map<String, Object> result = new HashMap<String, Object>();
 		
-		log.info("FileCount={}", form.getFiles().size());
-		
 		try {
 			//S3Utils 초기화
 			S3Utils.init();
+			
+			List<MultipartFile> uploadFiles = form.getFiles();
+			if(form.getFiles().size()==0) {
+				if(form.getParam00() != null) uploadFiles.add(form.getParam00());
+				if(form.getParam01() != null) uploadFiles.add(form.getParam01());
+				if(form.getParam02() != null) uploadFiles.add(form.getParam02());
+				if(form.getParam03() != null) uploadFiles.add(form.getParam03());
+				if(form.getParam04() != null) uploadFiles.add(form.getParam04());
+				if(form.getParam05() != null) uploadFiles.add(form.getParam05());
+				if(form.getParam06() != null) uploadFiles.add(form.getParam06());
+				if(form.getParam07() != null) uploadFiles.add(form.getParam07());
+				if(form.getParam08() != null) uploadFiles.add(form.getParam08());
+				if(form.getParam09() != null) uploadFiles.add(form.getParam09());
+
+				if(form.getParam10() != null) uploadFiles.add(form.getParam10());
+				if(form.getParam11() != null) uploadFiles.add(form.getParam11());
+				if(form.getParam12() != null) uploadFiles.add(form.getParam12());
+				if(form.getParam13() != null) uploadFiles.add(form.getParam13());
+				if(form.getParam14() != null) uploadFiles.add(form.getParam14());
+				if(form.getParam15() != null) uploadFiles.add(form.getParam15());
+				if(form.getParam16() != null) uploadFiles.add(form.getParam16());
+				if(form.getParam17() != null) uploadFiles.add(form.getParam17());
+				if(form.getParam18() != null) uploadFiles.add(form.getParam18());
+				if(form.getParam19() != null) uploadFiles.add(form.getParam19());
+			}
+			
+			log.info("FileCount={}", uploadFiles.size());
 			
 			String today = LocalDate.now().format(DateTimeFormatter.ofPattern("yyyyMMdd"));
 			String thumbnailSuffix = ".thumbnail.png";
@@ -59,8 +84,8 @@ public class UploadRestController {
 			String savedFoler = null;
 			String savedPath = null;
 			int seq = 0;
-			List<UploadFileVO> fileList = new ArrayList<UploadFileVO>();
-			for(MultipartFile mfile : form.getFiles()) {
+			List<UploadResultVO> fileList = new ArrayList<UploadResultVO>();
+			for(MultipartFile mfile : uploadFiles) {
 				String originFileName = mfile.getOriginalFilename();
 				
 				//썸네일인 경우 SKIP.
@@ -86,11 +111,11 @@ public class UploadRestController {
 				S3Utils.uploadFile(savedPath, mfile.getBytes(), mimeType);
 				
 				String viewUrl = String.format("%s%s", Constants.CDN_URL, savedPath);
-				UploadFileVO uploadInfo = UploadFileVO.of(originFileName, viewUrl, ((isVideo)? "video" : "image"), mimeType, seq);
+				UploadResultVO uploadInfo = UploadResultVO.of(originFileName, viewUrl, ((isVideo)? "video" : "image"), mimeType, seq);
 				
 				if(isVideo) {
 					String thumbnailName = String.format("%s%s", originFileName, thumbnailSuffix);
-					MultipartFile thumbnail = form.getFiles()
+					MultipartFile thumbnail = uploadFiles
 												.stream()
 												.filter(v -> v.getOriginalFilename().equals(thumbnailName))
 												.findFirst().orElse(null);
@@ -113,7 +138,7 @@ public class UploadRestController {
 		catch(Exception e) {
 			log.error("ERROR-upload...{}", e);
 			result.put("result", 500);
-			result.put("message", String.format("파일 처리중 오류가 발생하였습니다.\\n%s", e.getMessage()));
+			result.put("message", String.format("파일 처리중 오류가 발생하였습니다.(reason=%s)", e.getMessage()));
 		}
 
 		return result;
